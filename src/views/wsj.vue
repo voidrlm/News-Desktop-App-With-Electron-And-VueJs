e599d27dad5fbdf7318eea7f2d0ff4ca6e6ecd82
voidrlm <varjun609@gmail.com>
2023-01-26 03:51:57 +0530

    Implementation of wall street journal view

---
+++ src/views/wsj.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container fluid>
+    <v-card-title
+      :class="
+        $vuetify.breakpoint.mdAndUp ? 'text-h4 mt-n7' : 'text-h5 justify-center'
+      "
+      ><v-icon
+        size="35"
+        class="mr-2"
+        :color="$vuetify.theme.dark ? 'white' : 'black'"
+        >{{
+          getGreetingData === "Good Evening"
+            ? "mdi-moon-waning-crescent"
+            : "mdi-white-balance-sunny"
+        }}</v-icon
+      >{{ getGreetingData }}, User!</v-card-title
+    ><newsFeed :articles="newsFeed.data.articles" />
+  </v-container>
+</template>
+<script>
+import { fetchNewsData } from "@/services/apiCall";
+import newsFeed from "@/components/newsFeed.vue";
+export default {
+  components: { newsFeed },
+  name: "topheadlines-component",
+  data: () => ({
+    newsFeed: {},
+  }),
+  computed: {
+    getGreetingData() {
+      var today = new Date();
+      var curHr = today.getHours();
+
+      return curHr < 12
+        ? "Good Morning"
+        : curHr > 18
+        ? "Good Evening"
+        : "Good Afternoon";
+    },
+  },
+  created() {
+    fetchNewsData("everything?domains=wsj.com").then((response) => {
+      this.newsFeed = response;
+      console.log(response);
+    });
+
+    console.log(this.newsFeed);
+  },
+};
+</script>
