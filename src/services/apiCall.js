82d2a6b47e59d0e25bcfcb16cad0c8c60282966d
voidrlm <varjun609@gmail.com>
2023-02-04 11:16:51 +0530

    Changes

--- src/services/apiCall.js
+++ src/services/apiCall.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const baseUrl = "https://newsapi.org/v2/";
-const apiKey = "&apiKey=" + process.env.VUE_APP_API_KEY;
+const apiKey = "&apiKey=" + "a25d005bfaba46caade2c9ad228c260d";
 
 export const fetchNewsData = async (query) => {
   const response = await axios.get(baseUrl + query + apiKey);
