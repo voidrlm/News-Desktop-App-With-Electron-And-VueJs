f1f16ba1f0e0b9443de8023dd8b8a209065f3624
voidrlm <varjun609@gmail.com>
2023-02-04 10:58:05 +0530

    Defined public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
-})
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/News-Desktop-App-With-Electron-And-VueJs/"
+      : "/",
+  transpileDependencies: ["vuetify"],
+});
